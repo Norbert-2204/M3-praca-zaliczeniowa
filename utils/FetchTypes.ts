4a59a623576c6d1f6a5d85fc53d80488a0505006
Norbert <szasa60@gmail.com>
2026-01-17 17:28:20 +0100

    back with vercel

--- utils/FetchTypes.ts
+++ utils/FetchTypes.ts
@@ -1,7 +1,7 @@
 const getBaseUrl = () => {
-  // if (process.env.VERCEL_URL) {
-  //   return `https://${process.env.VERCEL_URL}`;
-  // }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
 
   if (process.env.NEXT_PUBLIC_SITE_URL) {
     return `https://${process.env.NEXT_PUBLIC_SITE_URL}`;
