67b37acf679bc8f573c84ff6dd716ca4420697a9
Norbert <szasa60@gmail.com>
2026-01-17 15:05:27 +0100

    add vercel

--- utils/FetchTypes.ts
+++ utils/FetchTypes.ts
@@ -63,8 +63,8 @@
 // export default FetchTypes;
 
 const getBaseUrl = () => {
-  if (process.env.NEXT_PUBLIC_SITE_URL) {
-    return `https://${process.env.NEXT_PUBLIC_SITE_URL}`;
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
   }
 
   return "http://localhost:3000";
