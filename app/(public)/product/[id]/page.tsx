1f6896f9b7670ca36b3752c4456ada8181089b05
Norbert <szasa60@gmail.com>
2026-01-18 08:27:39 +0100

    details client

--- app/(public)/product/[id]/page.tsx
+++ app/(public)/product/[id]/page.tsx
@@ -1,9 +1,6 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import ProtectPage from "@/components/ProtectPage";
-import ProductDetail from "@/components/product_page/ProductDetail";
-import FetchTypes from "@/utils/FetchTypes";
-import { Category } from "@/utils/Types";
+import ProductDetailClient from "@/components/product_page/DetailsClient";
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -17,25 +14,10 @@ const ProductDetailPage = async ({ params }: Props) => {
     return <p>Invalid product ID</p>;
   }
 
-  const { categoryRes } = await FetchTypes();
-
-  const res = await fetch(`$/api/product/${productId}`, {
-    cache: "no-store",
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch product");
-  }
-
-  const product = await res.json();
-
-  const categories: Category[] = categoryRes;
-  const category = categories.find((cat) => cat.id === product.categoryId);
-
   return (
     <>
       <Header />
-      <ProductDetail product={product} category={category} />
+      <ProductDetailClient productId={productId} />
       <Footer />
     </>
   );
