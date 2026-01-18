e73845b921dc231dd2b0bb1df87fd963a3aaa409
Norbert <szasa60@gmail.com>
2026-01-18 17:42:21 +0100

    test-1

--- components/home_page/Carousel.tsx
+++ components/home_page/Carousel.tsx
@@ -49,7 +49,7 @@ const Carousel = ({ categories, products }: FullProps) => {
   return (
     <div>
       <div className="px-6 xl:px-10 flex flex-col items-center">
-        <div className="relative flex flex-col dm:flex-row w-full h-[452px] overflow-hidden bg-[#222327] rounded border border-[#383B42]">
+        <div className="relative px-4 sm:px-0 flex flex-col dm:flex-row w-full h-[452px] overflow-hidden bg-[#222327] rounded border border-[#383B42]">
           <Button
             variant="iconBig"
             onClick={prev}
@@ -79,7 +79,7 @@ const Carousel = ({ categories, products }: FullProps) => {
               >
                 <div className="flex flex-col gap-6 max-w-[450px] text-center sm:text-left ">
                   <h2 className="text-[32px]">{capitalize(cat.name)}</h2>
-                  <p className="text-[#E7E7E7] px-2 sm:px-0">
+                  <p className="text-[#E7E7E7] ">
                     {capitalize(cat.exploreInfo)}
                   </p>
                   <div className="flex justify-center xl:justify-start">
@@ -90,7 +90,7 @@ const Carousel = ({ categories, products }: FullProps) => {
                     />
                   </div>
                 </div>
-                <div className="relative w-full max-w-[300px] xl:max-w-[450px] md:aspect-3/5 ">
+                <div className="relative h-[250px] sm:h-auto w-full max-w-[300px] xl:max-w-[450px] md:aspect-3/5 ">
                   <Image
                     src={products[idx].imageUrl}
                     alt={`category-${idx}`}
