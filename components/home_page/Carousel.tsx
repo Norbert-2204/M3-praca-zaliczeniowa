680f40b2bccd08d713f47ecfa7ba9c28f0c4f8de
Norbert <szasa60@gmail.com>
2026-01-20 19:00:30 +0100

    opacity

--- components/home_page/Carousel.tsx
+++ components/home_page/Carousel.tsx
@@ -54,13 +54,13 @@ const Carousel = ({ categories, products }: FullProps) => {
             variant="iconBig"
             onClick={prev}
             icon={<Left />}
-            className="absolute left-0 top-1/2 -translate-y-1/2 z-10 sm:py-4!"
+            className="absolute left-0 top-1/2 -translate-y-1/2 z-10 sm:opacity-50"
           />
           <Button
             variant="iconBig"
             onClick={next}
             icon={<Right />}
-            className="absolute right-0 top-1/2 -translate-y-1/2 z-10"
+            className="absolute right-0 top-1/2 -translate-y-1/2 z-10 sm:opacity-50"
           />
           <div
             className="flex h-[452px] transition-transform duration-500 ease-in-out"
