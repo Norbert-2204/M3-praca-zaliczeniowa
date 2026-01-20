c2fa41d8e7e218fde4c8bbb90ad6173aa4a358bf
Norbert <szasa60@gmail.com>
2026-01-20 20:12:20 +0100

    add filters

--- components/product_page/Sidebar.tsx
+++ components/product_page/Sidebar.tsx
@@ -32,6 +32,7 @@ const SideBar = ({ categories, brands }: SideBarProps) => {
   const { currency, setCurrency } = useCurrency();
 
   const [loadMore, setLoadMore] = useState(false);
+  const [visible, setVisible] = useState(false);
   const [mode, setMode] = useState<"category" | "brand">("category");
 
   const items = mode === "category" ? categories : brands;
@@ -93,89 +94,99 @@ const SideBar = ({ categories, brands }: SideBarProps) => {
 
   return (
     <aside className="max-w-[363px] min-w-[320px] p-4 sm:p-10 flex flex-col gap-10">
-      <Dropdown
-        variant="custom"
-        options={[
-          { label: "Category", value: "category" },
-          { label: "Brand", value: "brand" },
-        ]}
-        value={mode}
-        onChange={(val) => setMode(val as "category" | "brand")}
-        isDark
+      <Button
+        desc={visible ? "Hide filters" : "Show filters"}
+        onClick={() => setVisible(!visible)}
+        className="block lg:hidden"
+        sizes="average"
       />
-
-      <div className="flex flex-col gap-4 px-2">
-        <Input
-          variant="checkbox"
-          label="All"
-          checked={selectedItems.length === 0}
-          onChange={handleSelectAll}
-        />
-
-        {items.slice(0, loadMore ? undefined : 4).map((item) => (
-          <Input
-            key={item.id}
-            variant="checkbox"
-            label={item.name}
-            checked={selectedItems.includes(item.id)}
-            onChange={() => toggle(item.id)}
-          />
-        ))}
-
-        <Button
-          variant="ghost"
-          desc={loadMore ? "Load less" : "Load more"}
-          icon={loadMore ? <MinusSmall /> : <PlusSmall />}
-          onClick={() => setLoadMore((p) => !p)}
-          className="justify-start p-0 text-[#E7E7E7]"
-        />
-      </div>
-      <div className="flex flex-col gap-4 px-2.5">
+      <div
+        className={`p-4 sm:p-10 flex flex-col gap-10 ${visible ? "block" : "hidden"} lg:block`}
+      >
         <Dropdown
           variant="custom"
-          options={[{ label: "Price", value: "price" }]}
+          options={[
+            { label: "Category", value: "category" },
+            { label: "Brand", value: "brand" },
+          ]}
+          value={mode}
+          onChange={(val) => setMode(val as "category" | "brand")}
+          isDark
         />
-        <div className="flex items-start w-full">
+
+        <div className="flex flex-col gap-4 px-2">
           <Input
-            className="bg-[#262626] rounded-none max-w-[145px]"
-            sizes="dropdown"
-            placeholder="$ 10.00"
-            value={priceFrom ?? ""}
-            onChange={(e) => handlePriceFrom(e.target.value)}
+            variant="checkbox"
+            label="All"
+            checked={selectedItems.length === 0}
+            onChange={handleSelectAll}
           />
-          <Dropdown
-            variant="custom"
-            size="input"
-            options={[
-              { label: "USD", value: "USD" },
-              { label: "EUR", value: "EUR" },
-            ]}
-            value={currency}
-            onChange={(val) => setCurrency(val as "USD" | "EUR")}
-            fullWidth={false}
-            className="bg-[#262626]! border border-[#616674]"
+
+          {items.slice(0, loadMore ? undefined : 4).map((item) => (
+            <Input
+              key={item.id}
+              variant="checkbox"
+              label={item.name}
+              checked={selectedItems.includes(item.id)}
+              onChange={() => toggle(item.id)}
+            />
+          ))}
+
+          <Button
+            variant="ghost"
+            desc={loadMore ? "Load less" : "Load more"}
+            icon={loadMore ? <MinusSmall /> : <PlusSmall />}
+            onClick={() => setLoadMore((p) => !p)}
+            className="justify-start p-0 text-[#E7E7E7]"
           />
         </div>
-        <div className="flex items-start w-full">
-          <Input
-            className="bg-[#262626] rounded-none max-w-[145px]"
-            sizes="dropdown"
-            placeholder="$ 20.00"
-            value={priceTo ?? ""}
-            onChange={(e) => handlePriceTo(e.target.value)}
-          />
+        <div className="flex flex-col gap-4 px-2.5">
           <Dropdown
             variant="custom"
-            size="input"
-            options={[
-              { label: "USD", value: "USD" },
-              { label: "EUR", value: "EUR" },
-            ]}
-            value={currency}
-            onChange={(val) => setCurrency(val as "USD" | "EUR")}
-            fullWidth={false}
-            className="bg-[#262626]! border border-[#616674] "
+            options={[{ label: "Price", value: "price" }]}
           />
+          <div className="flex items-start w-full">
+            <Input
+              className="bg-[#262626] rounded-none max-w-[145px]"
+              sizes="dropdown"
+              placeholder="$ 10.00"
+              value={priceFrom ?? ""}
+              onChange={(e) => handlePriceFrom(e.target.value)}
+            />
+            <Dropdown
+              variant="custom"
+              size="input"
+              options={[
+                { label: "USD", value: "USD" },
+                { label: "EUR", value: "EUR" },
+              ]}
+              value={currency}
+              onChange={(val) => setCurrency(val as "USD" | "EUR")}
+              fullWidth={false}
+              className="bg-[#262626]! border border-[#616674]"
+            />
+          </div>
+          <div className="flex items-start w-full">
+            <Input
+              className="bg-[#262626] rounded-none max-w-[145px]"
+              sizes="dropdown"
+              placeholder="$ 20.00"
+              value={priceTo ?? ""}
+              onChange={(e) => handlePriceTo(e.target.value)}
+            />
+            <Dropdown
+              variant="custom"
+              size="input"
+              options={[
+                { label: "USD", value: "USD" },
+                { label: "EUR", value: "EUR" },
+              ]}
+              value={currency}
+              onChange={(val) => setCurrency(val as "USD" | "EUR")}
+              fullWidth={false}
+              className="bg-[#262626]! border border-[#616674] "
+            />
+          </div>
         </div>
       </div>
     </aside>
